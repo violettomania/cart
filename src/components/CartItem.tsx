4590bbaa0de954249e48cd96a14eb8ac338cae20
violettomania <danielpolya@protonmail.com>
2023-12-13 13:58:05 +0100

    handle adding and removing item in CartItem, handle negative values

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,6 +1,7 @@
 import { FaAngleUp, FaAngleDown } from 'react-icons/fa';
 import { useAppDispatch } from '../hooks/hooks';
 import { addToCart, removeFromCart } from '../slices/cartSlice';
+import { useState } from 'react';
 
 interface CartItemProps {
   title: string;
@@ -9,8 +10,26 @@ interface CartItemProps {
   amount: number;
 }
 
-export default function CartItem({ title, price, img, amount }: CartItemProps) {
+export default function CartItem({
+  title,
+  price,
+  img,
+  amount: amountProp,
+}: CartItemProps) {
+  const [amount, setAmount] = useState(amountProp);
   const dispatch = useAppDispatch();
+
+  const handleAddItem = () => {
+    setAmount(amount + 1);
+    dispatch(addToCart());
+  };
+
+  const handleRemoveItem = () => {
+    if (amount > 0) {
+      setAmount(amount - 1);
+      dispatch(removeFromCart());
+    }
+  };
 
   return (
     <article className='cart-item'>
@@ -21,14 +40,11 @@ export default function CartItem({ title, price, img, amount }: CartItemProps) {
         <button className='remove-btn'>remove</button>
       </div>
       <div>
-        <button className='amount-btn' onClick={() => dispatch(addToCart())}>
+        <button className='amount-btn' onClick={handleAddItem}>
           <FaAngleUp className='amount-icon' />
         </button>
         <span className='amount'>{amount}</span>
-        <button
-          className='amount-btn'
-          onClick={() => dispatch(removeFromCart())}
-        >
+        <button className='amount-btn' onClick={handleRemoveItem}>
           <FaAngleDown className='amount-icon' />
         </button>
       </div>
