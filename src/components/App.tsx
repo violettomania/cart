114abe9ff8b693c20d6cbee181f26290886e6feb
violettomania <danielpolya@protonmail.com>
2023-12-15 19:28:30 +0100

    add todo

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,6 +3,8 @@ import Cart from './Cart';
 import Navbar from './Navbar';
 import Spinner from './Spinner';
 
+// TODO: add wrapper with toggle hide/show navbar, cart
+
 export default function App() {
   const isLoading = useAppSelector((state) => state.cart.isLoading);
 
